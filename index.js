138f0de57ba57dad86d593e7d29c1e529088196e
Pectorel Maison <contact@devadra.com>
2023-07-09 23:00:55 +0200

    added lighten and darken functions + tests

--- index.js
+++ index.js
@@ -1,30 +1,94 @@
-/**
- * @type {import('postcss').PluginCreator}
- */
-module.exports = (opts = {}) => {
-  // Work with options here
+// Imports
+const valueParser = require('postcss-value-parser')
+const log = require('loglevel').getLogger('postcss-lighten-darken')
+const tinycolor = require('tinycolor2')
 
+// Logic
+
+function argsFromNode(node) {
+  return node.nodes.filter(function (node) {
+    return node.type === 'word' || node.type === 'function';
+  }).map(function (node) {
+    return node.type === 'function' ? node : node.value;
+  });
+}
+
+function lighten(value, percent) {
+  // Calculation based on this tool : https://mdigi.tools/darken-color/
+  percent = (percent*(1-tinycolor(value).toHsl().l)*100)/100;
+  return tinycolor(value).lighten(percent).toHexString();
+}
+
+function darken(value, percent) {
+  // Calculation based on this tool : https://mdigi.tools/lighten-color/
+  percent = (percent*((tinycolor(value).toHsl().l)*100))/100;
+  return tinycolor(value).darken(percent).toHexString();
+}
+
+function handleFunc(decl, postcss) {
+
+  const value = valueParser(decl.value).walk(node => {
+    if (node.type === 'function') {
+      let result = null;
+      //log.debug(node);
+
+      const args = argsFromNode(node);
+
+      const color = args[0];
+      const percentage = Number(args[1].replace('%', ''));
+
+      log.info("Initial color is :", color);
+
+      switch (node.value) {
+        case 'lighten':
+          result = lighten(color, percentage);
+          break;
+        case 'darken':
+          result = darken(color, percentage);
+          //result = darken(decl, node, postcss);
+          break;
+        default:
+          return;
+      }
+
+      if (!result) {
+        log.info(`Exiting after bad result.`);
+        return;
+      }
+
+      log.info("Return value is : ", result);
+
+      node.value = result;
+      node.nodes = null
+    }
+  }).toString();
+
+  log.debug('Setting decl value', value);
+  decl.value = value;
+
+}
+
+// Module export
+module.exports = (opts = {}) => {
+  opts = {
+    logLevel: 'debug',
+    ...opts
+  }
+
+  log.setLevel(opts.logLevel)
   return {
     postcssPlugin: 'postcss-dark-and-light',
-    /*
-    Root (root, postcss) {
-      // Transform CSS AST here
-    }
-    */
 
-    /*
-    Declaration (decl, postcss) {
-      // The faster way to find Declaration node
-    }
-    */
-
-    /*
-    Declaration: {
-      color: (decl, postcss) {
-        // The fastest way find Declaration node if you know property name
+    Declaration(decl, postcss) {
+      if (!decl.value.includes('lighten') && !decl.value.includes('darken')) {
+        return;
       }
+
+      handleFunc(decl, postcss);
+      //log.debug('decl = ', decl);
+
     }
-    */
+
   }
 }
 
--- index.test.js
+++ index.test.js
@@ -1,12 +1,54 @@
 const postcss = require('postcss')
+const dedent = require("dedent");
+const postcssSimpleVars = require('postcss-simple-vars')
 
 const plugin = require('./')
 
 async function run (input, output, opts = { }) {
-  let result = await postcss([plugin(opts)]).process(input, { from: undefined })
+  let { plugins, ...pluginOpts } = opts
+
+  if (plugins === undefined) {
+    plugins = [plugin(pluginOpts)];
+  }
+
+  let result = await postcss(plugins).process(input, { from: undefined })
   expect(result.css).toEqual(output)
   expect(result.warnings()).toHaveLength(0)
 }
+
+it('parses a var darken call', async () => {
+  await run(
+    dedent`
+    $primary-color: #c84754;
+
+    td { background: darken($primary-color, 40%); }
+    `,
+    dedent`
+    td { background: #7d252e; }
+    `,
+    {
+      logLevel: 'debug',
+      plugins: [postcssSimpleVars, plugin]
+    }
+  )
+})
+
+it('parses a var lighten call', async () => {
+  await run(
+    dedent`
+    $primary-color: #c84754;
+
+    td { background: lighten($primary-color, 40%); }
+    `,
+    dedent`
+    td { background: #de9198; }
+    `,
+    {
+      logLevel: 'debug',
+      plugins: [postcssSimpleVars, plugin]
+    }
+  )
+})
 
 /* Write tests here
 
